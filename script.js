30fa44d95e24c12107b08e4cceac953fd6e9f7c8
fin-goku <fingoku123@gmail.com>
2025-01-20 15:49:45 +0530

    rs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculateInflation() {
     const rateOfInflation = document.getElementById('rateOfInflation').value;
     const years = document.getElementById('years').value;
     const futureValue = initialAmount * Math.pow((1 + rateOfInflation / 100), years);
-    document.getElementById('inflationResult').textContent = `Future value: $${futureValue.toFixed(2)}`;
+    document.getElementById('inflationResult').textContent = `Future value: Rs.${futureValue.toFixed(2)}`;
 }
 
 function calculateSIP() {
@@ -12,7 +12,7 @@ function calculateSIP() {
     const years = document.getElementById('sipYears').value;
     const months = years * 12;
     const futureValue = monthlyInvestment * ((Math.pow((1 + rateOfReturn), months) - 1) / rateOfReturn) * (1 + rateOfReturn);
-    document.getElementById('sipResult').textContent = `Future value: $Rs.{futureValue.toFixed(2)}`;
+    document.getElementById('sipResult').textContent = `Future value: Rs.${futureValue.toFixed(2)}`;
 }
 
 function calculateCompoundInterest() {
@@ -20,5 +20,5 @@ function calculateCompoundInterest() {
     const rateOfInterest = document.getElementById('compoundRateOfInterest').value;
     const years = document.getElementById('compoundYears').value;
     const compoundInterest = principal * Math.pow((1 + rateOfInterest / 100), years);
-    document.getElementById('compoundResult').textContent = `Compound Interest: $${compoundInterest.toFixed(2)}`;
+    document.getElementById('compoundResult').textContent = `Compound Interest: Rs.${compoundInterest.toFixed(2)}`;
 }
